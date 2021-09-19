04e60f688d436fbaa57f27779d80bb3cde19d44e
Amirul Akmal <amirul.akmal.putrajaya2003@gmail.com>
2021-09-19 19:45:24 +0800

    Sprite Class

--- Starting SDL Graphic/src/Source.cpp
+++ Starting SDL Graphic/src/Source.cpp
@@ -8,6 +8,37 @@
 
 void capFrame(uint32_t Time);
 
+class Sprite
+{
+	SDL_Surface* image = NULL;
+	SDL_Rect rect;
+
+	int OriginX = 0, OriginY = 0;
+public:
+
+	Sprite(uint32_t color, int x, int y, int w = 64, int h = 48)
+	{
+		image = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
+		SDL_FillRect(image, NULL, color);
+
+		rect = image->clip_rect;
+		OriginX = rect.w / 2;
+		OriginY = rect.h / 2;
+		rect.x = x - OriginX;
+		rect.y = y - OriginY;
+	}
+
+	void update()
+	{
+		// later
+	}
+
+	void draw(SDL_Surface* Destination)
+	{
+		SDL_BlitSurface( image, NULL, Destination, &rect);
+	}
+
+};
 
 int main(int argc, char* argv[])
 {
@@ -37,8 +68,14 @@ int main(int argc, char* argv[])
 	// Get Window's Surface so we can modify it
 	SDL_Surface* Screen = SDL_GetWindowSurface(Window);
 	uint32_t color = SDL_MapRGB(Screen->format, 100, 200, 255);
+	uint32_t red = SDL_MapRGB(Screen->format, 255, 0, 0);
 	SDL_FillRect(Screen, NULL, color);
 
+	Sprite application(red, Wwidth / 2, Wheight / 2);
+	application.draw(Screen);
+
+	// To Update The Screen
+	// Any draw please put before this line
 	SDL_UpdateWindowSurface(Window);  
 
 	while (Running)
