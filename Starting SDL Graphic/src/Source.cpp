bcffe69016adb650d8aca8b8b9291d03e5ca2f1a
Amirul Akmal <amirul.akmal.putrajaya2003@gmail.com>
2021-09-19 18:20:41 +0800

    Update Surface Color

--- Starting SDL Graphic/src/Source.cpp
+++ Starting SDL Graphic/src/Source.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 
 	// SDL Window
 	// Must be a pointer
-	SDL_Window* Window;
+	SDL_Window* Window = NULL;
 
 	// Create Window
 	// Give the creation to the `Window Var`
@@ -21,13 +21,15 @@ int main(int argc, char* argv[])
 	SDL_Event event;
 	bool Running = true;
 
-	int x, y; // Top Left Coordinate
-	int w, h; // Size of the window from (x, y) Coord 
-
 	// For the Window Functionality:
-	// https://web.archive.org/web/20210210112300/http://wiki.libsdl.org/CategoryVideo
+	// https://wiki.libsdl.org/CategoryVideo
 
-	SDL_SetWindowTitle(Window, "Nama Saya ikan");
+	// Get Window's Surface so we can modify it
+	SDL_Surface* Screen = SDL_GetWindowSurface(Window);
+	uint32_t color = SDL_MapRGB(Screen->format, 100, 200, 255);
+	SDL_FillRect(Screen, NULL, color);
+
+	SDL_UpdateWindowSurface(Window);  
 
 	while (Running)
 	{
@@ -40,11 +42,7 @@ int main(int argc, char* argv[])
 				break;
 			}
 		}
-
-		SDL_GetWindowPosition(Window, &x, &y);
-		std::cout << "X: " << x << ",  Y :" << y << std::endl;
 	}
-	
 	SDL_DestroyWindow(Window);
 	return 1;
 }
