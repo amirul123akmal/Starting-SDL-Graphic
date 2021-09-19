1683b7b0d676aed8bfdf4b59aa8a493eb30054d5
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2021-09-19 14:16:26 +0800

    Main Loop and Event Catches

--- Starting SDL Graphic/src/Source.cpp
+++ Starting SDL Graphic/src/Source.cpp
@@ -18,7 +18,22 @@ int main(int argc, char* argv[])
 	// Give the creation to the `Window Var`
 	Window = SDL_CreateWindow("Main Application", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
 
-	SDL_Delay(3000);
+	SDL_Event event;
+	bool Running = true;
+
+	while (Running)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			// Detect if we press "X" to close our window
+			if (event.type == SDL_QUIT)
+			{
+				Running = false;
+				break;
+			}
+		}
+	}
+	
 	SDL_DestroyWindow(Window);
 	return 1;
 }
